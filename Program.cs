371b3d578cd0617f5812056b77d502cfc973d2e4
MichelleTaylorRG <michelle.taylor@red-gate.com>
2020-06-19 10:58:37 +0100

    Initialised bot

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace YetAnotherRpgBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            var discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = System.Environment.GetEnvironmentVariable("YarbToken"),
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
+            });
+
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("/yarb"))
+                    await e.Message.RespondAsync("I can't actually do things yet, sorry");
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
